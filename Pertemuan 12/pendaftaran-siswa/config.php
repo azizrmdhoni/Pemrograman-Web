b2ab6074460a8e13bc1cf71c6d48f9432dafdd53
azizrmdhoni <muhamadaziz389@gmail.com>
2025-11-22 14:51:04 +0700

    Create config.php

---
+++ Pertemuan 12/pendaftaran-siswa/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$server = "127.0.0.1:4306";
+$user = "root";
+$password = "";
+$nama_database = "upload_foto";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ) die("Gagal terhubung dengan database: " . mysqli_connect_error());
+
+
+?>
