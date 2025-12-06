6c68850ee2b61749549550cb7e951a91e8116f95
azizrmdhoni <muhamadaziz389@gmail.com>
2025-12-06 23:54:24 +0700

    Add footer.php with Bootstrap and alert handling

---
+++ Pertemuan 11/includes/footer.php
@@ -0,0 +1,20 @@
+</div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        function confirmDelete(message = 'Yakin ingin menghapus data ini?') {
+            return confirm(message);
+        }
+   
+        setTimeout(function() {
+            const alerts = document.querySelectorAll('.alert');
+            alerts.forEach(alert => {
+                const bsAlert = new bootstrap.Alert(alert);
+                bsAlert.close();
+            });
+        }, 3000);
+    </script>
+</body>
+</html>
